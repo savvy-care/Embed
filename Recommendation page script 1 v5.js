04743d4d35458e31469b99f52aa75f8df1a35f61
savvy-care <128630363+savvy-care@users.noreply.github.com>
2023-04-25 00:35:11 -0500

    Update Recommendation page script 1 v5.js

--- Recommendation page script 1 v5.js
+++ Recommendation page script 1 v5.js
@@ -22,8 +22,8 @@ let rec_reason_dict = {
 	"Fish Oil-1":"Because you eat < 2 servings of fish per week",
 	"Fish Oil-2":"To help with blood pressure or a heart health condition",
 	"Fish Oil-3":"To help with blood pressure or a heart health condition",
-	"B12-1":"Because vegan and vegetarian diets lack B12",
-	"B12-2":"Because you reported a deficiency",
+	"Vitamin B12-1":"Because vegan and vegetarian diets lack B12",
+	"Vitamin B12-2":"Because you reported a deficiency",
 	"Vitamin C-1":"Because smokers often have low vitamin C",
 	"Vitamin C-2":"Because you reported a deficiency",
 	"Turmeric-1":"To help with metabolic disorder, metabolic syndrome, or diabetes",
@@ -50,10 +50,10 @@ let deficiency_dict = {
 "Magnesium-2":"57% of men over 51 don't get enough<sup>1</sup>",
 "Magnesium-3":"48% of adult women don't get enough<sup>1</sup>",
 "Magnesium-4":"53% of women over 51 don't get enough<sup>1</sup>",
-"Fish Oil-1":"Most adult men get only 0.06g per day of EPA/DHA on average <sup>3</sup>",
-"Fish Oil-2":"Most men over 51 get only 0.07g per day of EPA/DHA on average <sup>3</sup>",
-"Fish Oil-3":"Most adult women get only 0.06g per day of EPA/DHA on average <sup>3</sup>",
-"Fish Oil-4":"Most women over 51 get only 0.06g per day of EPA/DHA on average <sup>3</sup>",
+"Fish Oil-1":"Up to 3g of DHA/EPA per day can produce benefit in adult men<sup>4</sup>",
+"Fish Oil-2":"Up to 3g of DHA/EPA per day can produce benefit in men over 51<sup>4</sup>",
+"Fish Oil-3":"Up to 3g of DHA/EPA per day can produce benefit in adult women<sup>4</sup>",
+"Fish Oil-4":"Up to 3g of DHA/EPA per day can produce benefit in women over 51<sup>4</sup>",
 "Folic Acid-1":"8% of adult men don't get enough<sup>1</sup>",
 "Folic Acid-2":"10% of men over 51 don't get enough<sup>1</sup>",
 "Folic Acid-3":"20% of adult women don't get enough<sup>1</sup>",
@@ -62,10 +62,10 @@ let deficiency_dict = {
 "Prenatal-2":"See why we recommend",
 "Prenatal-3":"See why we recommend",
 "Prenatal-4":"See why we recommend",
-"B12-1":"Only 4% of adult men are under the recommended amount<sup>1</sup>",
-"B12-2":"Only 5% of men over 51 don't get enough<sup>1</sup>",
-"B12-3":"8% of adult women don't get enough<sup>1</sup>",
-"B12-4":"9% of women over 51 don't get enough<sup>1</sup>",
+"Vitamin B12-1":"Only 4% of adult men are under the recommended amount<sup>1</sup>",
+"Vitamin B12-2":"Only 5% of men over 51 don't get enough<sup>1</sup>",
+"Vitamin B12-3":"8% of adult women don't get enough<sup>1</sup>",
+"Vitamin B12-4":"9% of women over 51 don't get enough<sup>1</sup>",
 "Iodine-1":"Iodine deficiency is uncommon.",
 "Iodine-2":"Iodine deficiency is uncommon.",
 "Iodine-3":"Iodine deficiency is uncommon.",
@@ -78,24 +78,16 @@ let deficiency_dict = {
 "Iron-2":"Less than 3% of men over 51 are below the recommended intake level<sup>1</sup>",
 "Iron-3":"19% of adult women get less than the recommended amount<sup>1</sup>",
 "Iron-4":"Less than 3% of women over 51 are below the recommended intake level<sup>1</sup>",
-"B1-1":"Only 4% of adult men don't get enough<sup>1</sup>",
-"B1-2":"5% of men over 51 don't get enough<sup>1</sup>",
-"B1-3":"10% of adult women don't get enough<sup>1</sup>",
-"B1-4":"13% of women over 51 don't get enough<sup>1</sup>",
-"B3-1":"Less than 3% of adult men don't get enough<sup>1</sup>",
-"B3-2":"Less than 3% of men over 51 don't get enough<sup>1</sup>",
-"B3-3":"Less than 3% of adult women don't get enough<sup>1</sup>",
-"B3-4":"4% of women over 51 don't get enough<sup>1</sup>",
-"B6-1":"4% of adult men don't get enough<sup>1</sup>",
-"B6-2":"16% of men over 51 don't get enough<sup>1</sup>",
-"B6-3":"10% of adult women don't get enough<sup>1</sup>",
-"B6-4":"19% of women over 51 don't get enough<sup>1</sup>",
+"Vitamin B Complex-1":"Inadequate intake of B1, B3, B6, and B12 ranges between 3-4% for adult men<sup>1</sup>",
+"Vitamin B Complex-2":"Inadequate intake of B1, B3, B6, and B12 ranges between 3-15% for men over 51<sup>1</sup>",
+"Vitamin B Complex-3":"Inadequate intake of B3, B6, and B12 ranges between 8-10% for adult women<sup>1</sup>",
+"Vitamin B Complex-4":"Inadequate intake of B1, B6, and B12 ranges between 9-19% for women over 51<sup>1</sup>",
 "Turmeric-1":"See why we recommend",
 "Turmeric-2":"See why we recommend",
 "Turmeric-3":"See why we recommend",
 "Turmeric-4":"See why we recommend"
 }
-let group = -1
+let group = -1;
 if (sex=="m"){
 	group = 1;
 	if (age >=51){
@@ -121,7 +113,7 @@ function assessRecReason(product,xx){
 			xx.getElementsByClassName("rec-reason")[0].innerText = rec_reason_dict[String(product)+"-"+String(fo_r)];
 		}
 	}
-	if (product =="B12"){
+	if (product =="Vitamin B12"){
 		if (b12r > 0) {
 			xx.getElementsByClassName("rec-reason")[0].innerText = rec_reason_dict[String(product)+"-"+String(b12r)];
 		}
@@ -150,7 +142,7 @@ function assessDeficiency(product,xx){
 if (code[8] == "1"){
 	product_list.push("Vitamin D");
 } if (code[8] == "2"){
-	product_list.push("Calcium");
+	product_list.push("Vitamin D + Calcium");
 } if (code[8] == "3"){
 	product_list.push("Vitamin D + Calcium");
 } if (code[7] == "1" || code[7] == "3"){
@@ -170,9 +162,9 @@ if (code[8] == "1"){
 } if (code[4] == "2" || code[4] == "3"){
 	product_list.push("Vitamin C");
 } if (code[3] == "1" || code[3] == "3"){
-	product_list.push("B6");
-} if (code[3] == "2" || code[3] == "3"){
-	product_list.push("B12");
+	product_list.push("Vitamin B Complex");
+} if (code[3] == "2"){
+	product_list.push("Vitamin B12");
 }
 
 let x = document.getElementsByClassName("collection-product");
